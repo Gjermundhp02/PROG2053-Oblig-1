cd859e789beb5688595673ca5735940d542338e0
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-09-23 18:32:56 +0200

    CSS fixes

--- Oblig 1/dynamc/index.js
+++ Oblig 1/dynamc/index.js
@@ -1,6 +1,6 @@
 async function* postElement() {
     let id = 1;
-    while(id++<=100){
+    while(id<=100){
         const element = document.createElement('div');
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
         if (response.ok) {
@@ -18,6 +18,7 @@ async function* postElement() {
                 yield element;
             }
         }
+        id++;
     }
 }
 
