f563b4387affb81accf6eaa17d217193452bb7a5
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-10-03 17:49:54 +0200

    Added more locations

--- RTdisplay/descriptions.js
+++ RTdisplay/descriptions.js
@@ -1,3 +1,4 @@
+// Made by https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
 const descriptions ={
     0: {
       day: {
--- RTdisplay/index.js
+++ RTdisplay/index.js
@@ -1,17 +1,52 @@
-async function update(){
-    const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=60.7957&longitude=10.6915&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms&timezone=Europe%2FBerlin&forecast_days=1')
-    const data = await res.json();
-    const el = document.getElementById('weather');
-    el.querySelector('img').src = descriptions[data.current.weather_code][data.current.is_day ? 'day' : 'night'].image;
-    el.querySelector('div>h1').textContent = "Gjøvik";
-    document.getElementById('temp').textContent = `${data.current.temperature_2m+data.current_units.temperature_2m}`;
-    document.getElementById('wind').textContent = `${data.current.wind_speed_10m+data.current_units.wind_speed_10m}`;
-    document.getElementById('precipitation').textContent = `${data.current.precipitation+data.current_units.precipitation}`;
-    document.getElementById('windDir').style.transform = `rotate(${data.current.wind_direction_10m}deg)`;
-    document.getElementById('windDir').style.width = '3rem';
-    el.querySelector('#wind>div>h2').textContent = data.current.wind_speed_10m+data.current_units.wind_speed_10m;
+const urls = {
+    Batman: "https://api.open-meteo.com/v1/forecast?latitude=37.8874&longitude=41.1322&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
+    Llanfairpwllgwyngyll: "https://api.open-meteo.com/v1/forecast?latitude=53.13277&longitude=4.11532&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
+    Wank: "https://api.open-meteo.com/v1/forecast?latitude=47.6152&longitude=10.5213&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
+    Kissing: "https://api.open-meteo.com/v1/forecast?latitude=48.3038&longitude=10.9709&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
+    Fucking: "https://api.open-meteo.com/v1/forecast?latitude=48.0674&longitude=12.8631&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms"
 }
 
-setInterval(update, 10000);
+function element(data, city){
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.insertAdjacentHTML("beforeend", `
+        <div>
+            <div>
+                <img src="${descriptions[data.current.weather_code][data.current.is_day ? 'day' : 'night'].image}">
+                <h1>${city}</h1>
+            </div>
+            <div>
+                <h2>${data.current.temperature_2m+data.current_units.temperature_2m}</h2>
+                <h2>${data.current.wind_speed_10m+data.current_units.wind_speed_10m}</h2>
+                <h2>${data.current.precipitation+data.current_units.precipitation}</h2>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img src="../images/south.png" id="windDir" style="transform:rotate(${data.current.wind_direction_10m}deg);">
+                <h2>${data.current.wind_speed_10m+data.current_units.wind_speed_10m}</h2>
+            </div>
+        </div>
+    `);
+    return div;
+}
+
+async function update(){
+    const container = document.getElementById('weather');
+    container.innerHTML = '';
+    const responses = await Promise.all(Object.values(urls).map(async url => fetch(url)));
+    const data = await Promise.all(responses.map(async response => {
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            throw new Error('Failed to fetch data');
+        }
+    }))
+    const elements = data.map((data, i) => element(data, Object.keys(urls)[i]));
+    container.replaceChildren(...elements);
+}
+
+setInterval(update, 20000);
 
 update();
