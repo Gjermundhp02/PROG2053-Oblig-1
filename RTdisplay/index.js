24e467a6cabbe8738654c7a31877b05a3905f82f
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-10-08 13:56:39 +0200

    Improved code

--- RTdisplay/index.js
+++ RTdisplay/index.js
@@ -1,3 +1,13 @@
+const url = "https://api.open-meteo.com/v1/forecast?latitude=37.8874,53.2214,47.6152,48.3038,48.0674&longitude=41.1322,-4.2033,10.5213,10.9709,12.8631&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms"
+
+const cities = [
+    "Batman",
+    "Llanfairpwllgwyng",
+    "Wank",
+    "Kissing",
+    "Fucking"
+]
+
 const urls = {
     Batman: "https://api.open-meteo.com/v1/forecast?latitude=37.8874&longitude=41.1322&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
     Llanfairpwllgwyngyll: "https://api.open-meteo.com/v1/forecast?latitude=53.13277&longitude=4.11532&current=temperature_2m,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms",
@@ -33,18 +43,26 @@ function element(data, city){
 
 async function update(){
     const container = document.getElementById('weather');
-    container.innerHTML = '';
-    const responses = await Promise.all(Object.values(urls).map(async url => fetch(url)));
-    const data = await Promise.all(responses.map(async response => {
-        if(response.ok){
-            return response.json()
-        }
-        else{
-            throw new Error('Failed to fetch data');
-        }
-    }))
-    const elements = data.map((data, i) => element(data, Object.keys(urls)[i]));
-    container.replaceChildren(...elements);
+    const response = await fetch(url);
+    if(response.ok){
+        const data = await response.json();
+        const elements = data.map((data, i) => element(data, cities[i]));
+        container.replaceChildren(...elements);
+    }
+    else{
+        throw new Error('Failed to fetch data');
+    }
+    // const responses = await Promise.all(Object.values(urls).map(async url => fetch(url)));
+    // const data = await Promise.all(responses.map(async response => {
+        // // if(response.ok){
+        // //     return response.json()
+        // // }
+        // // else{
+        // //     throw new Error('Failed to fetch data');
+    //     // }
+    // // }))
+    // // const elements = data.map((data, i) => element(data, Object.keys(urls)[i]));
+    // container.replaceChildren(...elements);
 }
 
 setInterval(update, 20000);
